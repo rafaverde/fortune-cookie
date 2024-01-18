73f53aedb666b226171defb6fe45fb0ecfebd1b7
Rafael Valverde <rafaverde@msn.com>
2024-01-17 22:11:31 -0300

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+<h1 align="center">Curso Explorer Stage 05 - RocketSeat - Projeto Biscoito da Sorte / Fortune Cookie</h1>
+
+<p align="center">
+Projeto desenvolvido no curso Explorer da Rocketseat, de uma aplicação simples com tecnologias HTML, CSS e JavaScript.
+</p>
+
+<p align="center">
+Veja o resultado <a href="" target="_blank">aqui.</a>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Cover" src=".github/preview.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+- Figma
+
+## 💻 Projeto
+
+Uma aplicaçõa em JavaScript e layout em HTML e CSS de um jogo simples para sortear frases de um biscoito da sorte, com efeitos de animação em CSS, funções e eventos em JS.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/community/file/1182751789348533739). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ by Rocketseat/[rafaverde](https://github.com/rafaverde) :wave: [Participe da nossa comunidade!](https://discord.gg/rocketseat)
---
+++ main.js
@@ -0,0 +1,49 @@
+// Phrases List
+const fortunePhrases = [
+  "A vida trará coisas boas se tiver paciência.",
+  "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+  "Não compense na ira o que lhe falta na razão.",
+  "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+  "A maior de todas as torres começa no solo.",
+  "Não há que ser forte. Há que ser flexível.",
+  "Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?",
+  "Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.",
+  "A juventude não é uma época da vida, é um estado de espírito.",
+  "Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.",
+]
+
+// Variables
+const playScreen = document.querySelector(".play-screen")
+const resultScreen = document.querySelector(".result-screen")
+const fortuneCookieClick = document.querySelector("#fortune_cookie")
+const resetButton = document.querySelector("#reset_button")
+let randomNumber = Math.round(Math.random() * (fortunePhrases.length - 1))
+
+// Events
+fortuneCookieClick.addEventListener("click", handleFortuneClick)
+resetButton.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", handleResetKey)
+
+// Functions
+function handleFortuneClick() {
+  let showPhrase = fortunePhrases[randomNumber]
+  resultScreen.querySelector("p").innerText = showPhrase
+  console.log(showPhrase, randomNumber)
+  toggleScreen()
+}
+
+function handleResetClick() {
+  randomNumber = Math.round(Math.random() * (fortunePhrases.length - 1))
+  toggleScreen()
+}
+
+function toggleScreen() {
+  playScreen.classList.toggle("hide")
+  resultScreen.classList.toggle("hide")
+}
+
+function handleResetKey(e) {
+  if (e.key == "Enter" && playScreen.classList.contains("hide")) {
+    handleResetClick()
+  }
+}
